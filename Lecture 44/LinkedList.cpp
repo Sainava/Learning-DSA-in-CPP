e994ec9a96d0f4855fc2652463f181c58f588d2a
Sainava <sainava.modak@gmail.com>
2025-06-11 19:30:35 +0530

    Singly Linked List

--- Lecture 44/LinkedList.cpp
+++ Lecture 44/LinkedList.cpp
@@ -12,10 +12,194 @@ class Node {
     }
 };
 
+
+
+void AddatTail(Node * tail,int data){
+    Node * newNode=new Node(data);
+    if(tail==NULL){
+        tail==newNode;
+        return;
+    }
+    tail->next=newNode;
+    tail=newNode;
+
+}
+
+void printList(Node * &Head){
+    Node * temp=Head;
+    while(temp !=NULL){
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+    cout << endl;
+}
+
+void InsertAtHead(Node* &Head,int data){
+    if(Head==NULL){
+        Head=new Node(data);
+        return;
+    }
+    Node * newNode=new Node(data);
+    newNode->next=Head;
+    Head=newNode;
+}
+
+void InsertAtTail(Node* &Head,int data){
+    Node * newNode=new Node(data);
+    if(Head==NULL){
+        Head=newNode;
+        return;
+    }
+    Node * temp=Head;
+    while(temp->next!=NULL){
+
+        temp=temp->next;
+    }
+    temp->next=newNode;
+}
+
+void InsertAtPosition(Node* &Head,int data,int pos){
+    if(pos==0){
+        InsertAtHead(Head,data);
+        return;
+    }
+    Node* newNode=new Node(data);
+    Node* temp= Head;
+    for(int i=1;i<pos-1 && temp!=NULL;i++){
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        cout << "Position out of bounds, inserting at tail instead." << endl;
+        InsertAtTail(Head, data);
+        return;
+    }
+
+    newNode->next=temp->next;
+
+    temp->next=newNode;
+}
+
+void deleteHead(Node * Head){
+    if(Head==NULL){
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+    Node * temp=Head;
+    Head=Head-> next;
+    delete temp;
+}
+
+void deleteTail(Node * Head){
+    if(Head==NULL){
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+    Node * temp=Head;
+    if(temp->next=NULL){
+        delete temp;
+        Head=NULL;
+        return;
+    }
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    delete temp->next;
+    temp->next=NULL;
+}
+void deleteAtPosition(Node * & Head,int pos){
+    if(Head==NULL){
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+    if(pos==1){
+        Node * temp=Head;
+        Head=Head->next;
+        delete temp;
+        return;
+    }
+
+    Node * temp=Head;
+    for(int i=1;i<pos-1 && temp!=NULL;i++){
+        temp=temp->next;
+    }    
+    if(temp==NULL || temp->next==NULL){
+        cout << "Position out of bounds, nothing to delete." << endl;
+        return;
+    }
+
+    Node * toDelete=temp->next;
+    temp->next=toDelete->next;
+    delete toDelete;
+}
+
+void deleteusingValue(Node * Head,int value ){
+    if(Head==NULL){
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+    if(Head->data==value){
+        Node * temp=Head;
+        Head=Head->next;
+        delete temp;
+        return;
+    }
+    Node * temp=Head;
+    while(temp->next!=NULL && temp->next->data!=value){
+        temp=temp->next;
+    }
+    if(temp->next==NULL){
+        cout << "Value not found, nothing to delete." << endl;
+        return;
+    }
+    Node * toDelete=temp->next;
+    temp->next=toDelete->next;
+    delete toDelete;
+}
+
+void  deleteList(Node * &Head){
+    if(Head==NULL){
+        cout << "List is already empty." << endl;
+        return;
+    }
+    Node * temp;
+    while(Head != NULL){
+        temp = Head;
+        Head = Head->next;
+        delete temp;
+    }
+    cout << "List deleted successfully." << endl;
+}
+
+void updateValue(Node * Head, int oldValue, int newValue) {
+    if (Head == NULL) {
+        cout << "List is empty, nothing to update." << endl;
+        return;
+    }
+    Node * temp = Head;
+    while(temp!=NULL){
+        if(temp->data==oldValue){
+            temp->data=newValue;
+            cout << "Value updated successfully." << endl;
+            return; 
+        }
+    }
+    if(temp==NULL){
+        cout << "Value not found, nothing to update." << endl;
+        return;
+    }
+}
+    
+
 int main(){
    Node * head= new Node(10);
 
    cout << "Head data: " << head->data << endl;
    cout << "Head next: " << head->next << endl;
+   InsertAtHead(head,1);
+   InsertAtTail(head,20);
+   printList(head);
+   InsertAtPosition(head, 15, 3);
+    printList(head);
+
    return 0;
 }
