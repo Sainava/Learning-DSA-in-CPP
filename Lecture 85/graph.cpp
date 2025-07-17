e01b771a258e2b5d877951319f2e98e4a149c63b
Sainava <sainava.modak@gmail.com>
2025-07-17 16:51:23 +0530

    Graph intro

---
+++ Lecture 85/graph.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// This is a placeholder for the graph.cpp file
+// The actual implementation of the graph will be added later
+
+class Graph {
+public: 
+    // Graph data structure can be defined here
+    // For example, using an adjacency list or matrix
+
+    Graph() {
+        // Constructor to initialize the graph
+    }
+
+    void addEdge(int u, int v) {
+        // Function to add an edge between vertices u and v
+    }
+
+    void display() {
+        // Function to display the graph
+    }
+};
+
+// Additional functions for graph operations can be added here
+// For example, BFS, DFS, shortest path algorithms, etc.
+
+int main(){
+   cout << "Hello, Graph!" << endl;
+
+    return 0;
+}
