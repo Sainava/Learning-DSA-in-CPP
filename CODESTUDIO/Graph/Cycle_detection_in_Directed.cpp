d2ade90cf49866cffb1d9cdaccd443f58d43cf33
Sainava <sainava.modak@gmail.com>
2025-08-24 14:06:08 +0530

    Cycle Detection in directed graph using topological sort or Kahn's Algo

--- CODESTUDIO/Graph/Cycle_detection_in_Directed.cpp
+++ CODESTUDIO/Graph/Cycle_detection_in_Directed.cpp
@@ -11,6 +11,7 @@ Find whether the graph contains a cycle or not, return true if a cycle is presen
 using namespace std;
 
 
+//Solution 1: DFS
 int dfsCycle(int node,unordered_map<int,bool> &visited, unordered_map<int,bool> &dfs_visited,unordered_map<int,vector<int>> &adj){
     visited[node]=true;
     dfs_visited[node]=true;
@@ -64,4 +65,72 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
   //TC:O(V+E)
   //SC:O(V+E)
   return 0;
+}
+
+//Solution 2: BFS or Kahn's algo
+#include<queue>
+#include<unordered_map>
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+   //Cycle detection using Topological Sort using Kahn's Algo
+
+    //Create the adj list
+    //SC:O(V+E)
+    unordered_map<int,vector<int>> adj;
+
+    //TC:O(E)
+    for(auto p:edges){
+        int u=p.first-1;
+        int v=p.second-1; // Since in this question , the node starts from 1 ,so we make them start from zero so we can use our earlier logic 
+
+        adj[u].push_back(v);
+    }
+
+    //Create the indegree list
+    vector<int> indegree(n);
+
+    //TC:O(V+E)
+    for(auto pairs:adj){
+        for(auto neighbour:pairs.second){
+            indegree[neighbour]++;
+        }
+    }
+
+    queue<int> q;
+
+    //Push the nodes with indegree 0 into the queue
+
+    //TC:O(V)
+    for(int i=0;i<n;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+
+    int count=0;
+
+    //Do the Modified BFS
+    //TC:O(V+E)
+    while(!q.empty()){
+        int frontNode=q.front();
+        q.pop();
+
+        count++;
+
+        for(auto neighbour:adj[frontNode]){
+            indegree[neighbour]--;
+            if(indegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+    }
+
+    //TC:O(V+E)
+    //SC:O(V+E)
+    if(count==n){
+        return false;
+        //All elements traversed , valid  topological sort  -> no cycle
+    }
+
+    return true;
 }
