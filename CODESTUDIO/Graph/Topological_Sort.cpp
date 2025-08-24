70f75bd283adda68d680874dee73dedb00cfa380
Sainava <sainava.modak@gmail.com>
2025-08-24 12:38:43 +0530

    Topological sort using DFS

---
+++ CODESTUDIO/Graph/Topological_Sort.cpp
@@ -0,0 +1,67 @@
+/*
+Problem statement
+A Directed Acyclic Graph (DAG) is a directed graph that contains no cycles.
+
+Topological Sorting of DAG is a linear ordering of vertices such that for every directed edge from vertex ‘u’ to vertex ‘v’, vertex ‘u’ comes before ‘v’ in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+
+Given a DAG consisting of ‘V’ vertices and ‘E’ edges, you need to find out any topological sorting of this DAG. Return an array of size ‘V’ representing the topological sort of the vertices of the given DAG.
+*/
+
+#include<iostream>
+#include<unordered_map>
+#include<stack>
+#include<vector>
+
+using namespace std;
+
+//Solution 1 --> USING DFS
+
+void dfs_topology(int node,unordered_map<int,bool> &visited,unordered_map<int,vector<int>> &adj,stack<int> &st){
+    visited[node]=true;
+
+    for(auto neighbour:adj[node]){
+        if(!visited[neighbour]){
+            dfs_topology(neighbour,visited,adj,st);
+        }
+    }
+
+    st.push(node);
+}
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Create the adj list
+    //SC:O(V+E)
+    unordered_map<int,vector<int>> adj;
+
+    //Topological sort can only be applied on Directed Acyclic Graph(DAG)
+    //TC:O(E)
+    for(auto p:edges){
+        int u=p[0];
+        int v=p[1];
+
+        adj[u].push_back(v);
+    }
+
+    //TC:O(V)
+    unordered_map<int,bool> visited;
+    stack<int> st;
+
+    //TC:O(V+E)
+    for(int i=0;i<v;i++){
+        if(!visited[i]){
+            dfs_topology(i,visited,adj,st);
+        }
+    }
+
+    vector<int> ans;
+
+    //TC:O(V)
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+
+    //TC:O(V+E)
+    //SC:O(V+E)
+    return ans;
+}
