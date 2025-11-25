8b922f1c7a7e3c283c616ab0fd5bb0af8fc3f5c7
Sainava <sainava.modak@gmail.com>
2025-11-26 01:50:22 +0530

    0/1 knapsack DP

--- CODESTUDIO/Dynamic Programming/2D DP/0_1_Kanpsack.cpp
+++ CODESTUDIO/Dynamic Programming/2D DP/0_1_Kanpsack.cpp
@@ -10,7 +10,7 @@ A thief is robbing a store and can carry a maximal weight of W into his knapsack
 
 using namespace std;
 
-//Solution 1
+//Solution 1 -- Top-Down Approach (Recursion + Memoization)
 int solve(vector<int> weight, vector<int> value, int index, int capacity,vector<vector<int>> &dp){
 	//Base Case
 	if(index==0){
@@ -52,4 +52,123 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	//TC:O(n x maxWeight)
 	//SC:O(n x maxWeight)
 	return solve(weight,value,n-1,maxWeight,dp);
+}
+
+
+//solve 2 -- Bottom-Up Approach (Tabulation)
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	vector<vector<int>> dp(n,vector<int> (maxWeight+1,-1));
+
+	//Base condition  --> That is only one item with us 
+	for(int wt=0;wt<=maxWeight;wt++){
+		if(weight[0]<=wt){
+			dp[0][wt]=value[0];
+		}
+		else{
+			dp[0][wt]=0;
+		}
+	}
+
+	for(int i=1;i<n;i++){
+		//include
+		for(int wt=0;wt<=maxWeight;wt++){
+
+			//include 
+			int include=0;
+			if(weight[i]<=wt){
+				include=value[i] + dp[i-1][wt-weight[i]];
+			}
+
+			//exclude
+			int exclude=dp[i-1][wt];
+
+			dp[i][wt]=max(include,exclude);
+
+		}
+	}
+
+	//TC and SC:O(n*maxWeight)
+	return dp[n-1][maxWeight];
+}
+
+
+//Solve 3: Space Optimization
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	
+	vector<int> prev(maxWeight+1,-1);
+	vector<int> curr(maxWeight+1,-1);
+
+	//Base condition  --> That is only one item with us 
+	for(int wt=0;wt<=maxWeight;wt++){
+		if(weight[0]<=wt){
+			prev[wt]=value[0];
+		}
+		else{
+			prev[wt]=0;
+		}
+	}
+
+	for(int i=1;i<n;i++){
+		//include
+		for(int wt=0;wt<=maxWeight;wt++){
+
+			//include 
+			int include=0;
+			if(weight[i]<=wt){
+				include=value[i] + prev[wt-weight[i]];
+			}
+
+			//exclude
+			int exclude=prev[wt];
+
+			curr[wt]=max(include,exclude);
+
+		}
+
+		prev=curr;
+	}
+
+	//TC:O(n*maxWeight)
+	//SC:O(maxWeight)
+	return prev[maxWeight];
+}
+
+// Solution 4: Further Space Optimization
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	vector<int> curr(maxWeight+1,-1);
+
+	//Base condition  --> That is only one item with us 
+	for(int wt=0;wt<=maxWeight;wt++){
+		if(weight[0]<=wt){
+			curr[wt]=value[0];
+		}
+		else{
+			curr[wt]=0;
+		}
+	}
+
+	for(int i=1;i<n;i++){
+		//include
+		for(int wt=maxWeight;wt>=0;wt--){
+
+			//include 
+			int include=0;
+			if(weight[i]<=wt){
+				include=value[i] + curr[wt-weight[i]];
+			}
+
+			//exclude
+			int exclude=curr[wt];
+
+			curr[wt]=max(include,exclude);
+
+		}
+	}
+
+	//TC:O(n*maxWeight)
+	//SC:O(maxWeight)
+	return curr[maxWeight];
 }
