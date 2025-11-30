94416a65bde4e30c659fd5ebe14465b2992638de
Sainava <sainava.modak@gmail.com>
2025-11-30 21:41:23 +0530

    Insertion Sort

---
+++ CODESTUDIO/Sort/Insertion_sort.cpp
@@ -0,0 +1,26 @@
+/*
+Problem statement
+You are given ‘N’ integers in the form of an array ‘ARR’. Print the sorted array using the insertion sort.
+
+Note :
+No need to return anything. You should sort the array in-place.
+For example :
+Let ‘ARR’ be: [1, 4, 2]
+The sorted array will be: [1, 2, 4].
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    for(int i=1;i<n;i++){
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>temp){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+}
